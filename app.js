3308f425fd994230e0285ef221ce63d07bb977a4
tomfox747 <thomasfox747@gmail.com>
2021-02-18 17:38:13 +0000

    added full authentication based on origin

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const app = express();
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
+const authOrigin = require('./middleware/authoriseOrigin')
+
 const healthcheck = require('./routes/healthcheck.js');
 const data_router = require('./routes/data_router.js');
 
 app.use(bodyParser.json());
 app.use(cors());
+console.log("completed middleware");
 app.use('/healthcheck', healthcheck);
-app.use('/web/data', data_router);
+app.use('/web/data', authOrigin, data_router);
 
 module.exports = app;
